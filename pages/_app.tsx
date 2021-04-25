ad339fba6909dfd3c56b9875caac49f499b5860c
Niels Bik <nielsrowinbik@gmail.com>
2021-04-25 20:58:44 +0200

    Add more of the usual PWA prerequisites

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,41 @@
+import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
+
 import '../styles/globals.css';
 
 import Container from '../components/Container';
 
-import { ThemeProvider } from 'next-themes';
+const useServiceWorker = () => {
+    const [isWaiting, setWaiting] = useState(false);
+
+    let wb;
+    const skipWaiting = () => wb?.messageSkipWaiting();
+
+    if (
+        typeof window !== 'undefined' &&
+        'serviceWorker' in navigator &&
+        window.workbox !== undefined
+    ) {
+        wb = window.workbox;
+
+        wb.addEventListener('waiting', () => setWaiting(true));
+        wb.addEventListener('controlling', () => window.location.reload());
+    }
+
+    return {
+        isWaiting,
+        skipWaiting,
+    };
+};
 
 export default function App({ Component, pageProps }) {
+    const { isWaiting, skipWaiting } = useServiceWorker();
+
+    useEffect(() => {
+        // TODO: Ask user if he wants to refresh the page to get the latest version
+        // TODO: In the onClick handler of whatever we use to ask the question, call `skipWaiting`
+    }, [isWaiting]);
+
     return (
         <ThemeProvider attribute="class">
             <Container>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,103 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-// TODO: Add icons, fonts, etc here.
-
 class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head></Head>
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <title>Niels Bik - Product Manager from Utrecht</title>
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="57x57"
+                        href="/assets/icons/apple-touch-icon-57x57.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="60x60"
+                        href="/assets/icons/apple-touch-icon-60x60.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="72x72"
+                        href="/assets/icons/apple-touch-icon-72x72.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="76x76"
+                        href="/assets/icons/apple-touch-icon-76x76.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="114x114"
+                        href="/assets/icons/apple-touch-icon-114x114.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="120x120"
+                        href="/assets/icons/apple-touch-icon-120x120.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="144x144"
+                        href="/assets/icons/apple-touch-icon-144x144.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="152x152"
+                        href="/assets/icons/apple-touch-icon-152x152.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/assets/icons/apple-touch-icon-180x180.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/assets/icons/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="194x194"
+                        href="/assets/icons/favicon-194x194.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="192x192"
+                        href="/assets/icons/android-chrome-192x192.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/assets/icons/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link
+                        rel="mask-icon"
+                        href="/assets/icons/safari-pinned-tab.svg"
+                        color="#ffffff"
+                    />
+                    <meta
+                        name="description"
+                        content="Product Manager from Utrecht currently working for ProRail"
+                    />
+                    <meta name="msapplication-TileColor" content="#383c43" />
+                    <meta
+                        name="msapplication-TileImage"
+                        content="/assets/icons/mstile-144x144.png"
+                    />
+                    <meta name="theme-color" content="#ffffff" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    />
+                </Head>
                 <body className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white dark:text-opacity-90">
                     <Main />
                     <NextScript />
