b0892eef066731db7961007805e60aa75d85445b
Niels Bik <nielsrowinbik@gmail.com>
2021-04-25 21:15:47 +0200

    Ignore .env file (oops!), add instructions on how to get or cycle Spotify tokens

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,5 +1,16 @@
 import querystring from 'querystring';
 
+// Refer to https://documenter.getpostman.com/view/583/spotify-playlist-generator/2MtDWP to get a refresh token.
+// 1. Open Postman and create a new request
+// 2. Under Authorization, choose OAuth2.0
+// 3. Fill in the following:
+//    - Auth URL: https://accounts.spotify.com/authorize
+//    - Access Token URL: https://accounts.spotify.com/api/token
+//    - Client ID
+//    - Client Secret:
+//    - Scope: user-read-currently-playing
+// 4. Authorize the request with your account, and get the Refresh Token from Postman
+
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
