644e605bd7ca078df8b9942917292482a758b29e
Niels Bik <nielsrowinbik@gmail.com>
2021-08-15 16:26:55 +0200

    Follow system for dark mode, remove button

--- components/Container.tsx
+++ components/Container.tsx
@@ -54,23 +54,6 @@ export default function Container(props) {
                 {/* <div>
                     <NextLink href="/">Home</NextLink>
                 </div> */}
-                <button
-                    aria-label="Toggle Dark Mode"
-                    type="button"
-                    className="bg-transparent rounded h-14 w-14 flex justify-center items-center opacity-70"
-                    onClick={() =>
-                        setTheme(theme === 'dark' ? 'light' : 'dark')
-                    }
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 48 48"
-                        fill="currentColor"
-                        className="h-6 w-6 text-gray-800 dark:text-gray-200"
-                    >
-                        <path d="M22 41C32.4934 41 41 32.4934 41 22C41 11.5066 32.4934 3 22 3C11.5066 3 3 11.5066 3 22C3 32.4934 11.5066 41 22 41ZM7 22C7 13.7157 13.7157 7 22 7V37C13.7157 37 7 30.2843 7 22Z" />
-                    </svg>
-                </button>
             </nav>
             <main
                 id="skip"
