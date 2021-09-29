55fa2a65734d865e8efd8ccf2131e66626fe212d
Niels Bik <nielsrowinbik@gmail.com>
2021-09-29 16:43:46 +0200

    Make Now Playing pulse at the BPM of the playing track

---
+++ lib/types.ts
@@ -0,0 +1,12 @@
+export type NowPlayingResponse = {
+    album: string;
+    albumImageUrl: string;
+    albumUrl: string;
+    artist: string;
+    artistUrl: string;
+    isPlaying: boolean;
+    songBps: number;
+    songTimeSignature: number;
+    songUrl: string;
+    title: string;
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,30 @@
 const { trueGray } = require('tailwindcss/colors');
 const { colors } = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
     darkMode: 'media',
     mode: 'jit',
-    plugins: [require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/typography'),
+        plugin(function ({ addVariant, e }) {
+            addVariant('empty', ({ modifySelectors, separator }) => {
+                modifySelectors(({ className }) => {
+                    return `.${e(`empty${separator}${className}`)}:empty`;
+                });
+            });
+        }),
+    ],
     purge: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
     theme: {
         colors: {
             ...colors,
             gray: trueGray,
+        },
+        extend: {
+            colors: {
+                'spotify-green': '#1ed760',
+            },
         },
     },
     variants: {
