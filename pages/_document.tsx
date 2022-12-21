93786eab52c091f65409eb7b00bd6c2b49f98d0c
Niels Bik <nielsrowinbik@gmail.com>
2022-12-21 21:52:10 +0100

    fix: prevent white flash upon first page load

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,12 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => (
-  <Html lang="en">
+  <Html className="bg-white dark:bg-zinc-900" lang="en">
     <Head>
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     </Head>
-    <body className="min-h-screen bg-white dark:bg-zinc-900">
+    <body className="min-h-screen">
       <Main />
       <NextScript />
     </body>
