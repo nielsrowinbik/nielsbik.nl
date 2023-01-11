99173fb5328c31f2e24736c16c6e34dc11fe5acb
Niels Bik <nielsrowinbik@gmail.com>
2023-01-11 11:21:37 +0100

    feat: improve lighthouse score

--- components/HomeLayout.tsx
+++ components/HomeLayout.tsx
@@ -8,7 +8,7 @@ export function HomeLayout({ children, className }: HomeLayoutProps) {
     <div className="grid place-content-around">
       <main
         className={cn(
-          "prose prose-zinc m-8 max-w-md prose-a:font-normal prose-a:text-blue-500 prose-a:no-underline hover:prose-a:underline prose-hr:my-8 last:prose-hr:hidden dark:prose-invert dark:prose-a:text-blue-400 sm:mt-24 lg:mt-36 xl:mt-48",
+          "prose prose-zinc m-8 max-w-md prose-a:font-normal prose-a:text-blue-600 prose-a:no-underline hover:prose-a:underline prose-hr:my-8 last:prose-hr:hidden dark:prose-invert dark:prose-a:text-blue-400 sm:mt-24 lg:mt-36 xl:mt-48",
           className
         )}
       >
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,24 @@ const Document = () => (
   <Html className="bg-white dark:bg-zinc-900" lang="en">
     <Head>
       <meta charSet="utf-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
     <body className="min-h-screen">
       <Main />
