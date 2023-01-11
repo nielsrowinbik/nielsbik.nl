b3f16b51a29d7fcda4c291378886d266574c8144
Niels Bik <nielsrowinbik@gmail.com>
2023-01-11 11:21:37 +0100

    feat: improve lighthouse score

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { MDXComponents } from "@/components/MDXComponents";
 import { MDXProvider } from "@mdx-js/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <MDXProvider components={MDXComponents}>
+      <Head>
+        <meta name="viewport" content="width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </MDXProvider>
   );
