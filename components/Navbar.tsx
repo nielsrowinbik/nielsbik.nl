2a7df466ae6c0fcc8eb6fe40773ba9d4cdc109a9
Niels Bik <nielsrowinbik@gmail.com>
2023-03-03 17:31:39 +0100

    refactor: move to framer motion layoutgroup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,36 +1,17 @@
 "use client";
 
+import { LayoutGroup, motion } from "framer-motion";
+
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 const navItems: {
-  [path: string]: {
-    name: string;
-    x: number;
-    y: number;
-    w: string;
-  };
+  [path: string]: string;
 } = {
-  "/": {
-    name: "home",
-    x: 0,
-    y: 0,
-    w: "64px",
-  },
-  "/about": {
-    name: "about",
-    x: 64,
-    y: 35,
-    w: "65px",
-  },
-  "/blog": {
-    name: "blog",
-    x: 127,
-    y: 69,
-    w: "56px",
-  },
+  "/": "home",
+  "/about": "about",
+  "/blog": "blog",
 };
 
 export function Navbar() {
@@ -43,73 +24,47 @@ export function Navbar() {
   return (
     <aside className="-mx-4 font-serif md:mx-0 md:w-[150px] md:flex-shrink-0 md:px-0">
       <div className="lg:sticky lg:top-20">
-        <nav
-          className="relative flex scroll-pr-6 flex-row items-start overflow-scroll px-4 pb-0 md:relative md:flex-col md:overflow-auto md:px-0"
-          id="nav"
-        >
-          <div className="mb-2 mt-2 flex flex-row space-x-0 pr-10 md:mt-0 md:flex-col">
-            {navItems[pathname] ? (
-              <>
-                {/* Desktop version, hidden on mobile, animates y axis */}
-                <div className="hidden md:block">
-                  <motion.div
-                    className="absolute z-[-1] h-[34px] rounded-md bg-neutral-100 dark:bg-neutral-800"
-                    layoutId="test2"
-                    initial={{ opacity: 0, y: navItems[pathname].y }}
-                    animate={{
-                      opacity: 1,
-                      y: navItems[pathname].y,
-                      width: navItems[pathname].w,
-                    }}
-                    transition={{
-                      type: "spring",
-                      stiffness: 350,
-                      damping: 30,
-                    }}
-                  />
-                </div>
-                {/* Mobile version, hidden on desktop, animates x axis */}
-                <div className="block md:hidden">
-                  <motion.div
-                    className="absolute z-[-1] h-[34px] rounded-md bg-neutral-100 dark:bg-neutral-800"
-                    layoutId="test"
-                    initial={{ opacity: 0, x: navItems[pathname].x }}
-                    animate={{
-                      opacity: 1,
-                      x: navItems[pathname].x,
-                      width: navItems[pathname].w,
-                    }}
-                    transition={{
-                      type: "spring",
-                      stiffness: 350,
-                      damping: 30,
-                    }}
-                  />
-                </div>
-              </>
-            ) : null}
+        <LayoutGroup>
+          <nav
+            className="fade relative flex scroll-pr-6 flex-row items-start overflow-scroll px-4 pb-0 md:relative md:flex-col md:overflow-auto md:px-0"
+            id="nav"
+          >
+            <div className="mb-2 mt-2 flex flex-row space-x-0 pr-10 md:mt-0 md:flex-col">
+              {Object.entries(navItems).map(([path, name]) => {
+                const isActive = path === pathname;
 
-            {Object.entries(navItems).map(([path, { name }]) => {
-              const isActive = path === pathname;
-
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className={cn(
-                    "py-[5px] px-[10px] transition-all hover:text-neutral-800 dark:hover:text-neutral-200",
-                    {
-                      "text-neutral-500": !isActive,
-                      "font-bold": isActive,
-                    }
-                  )}
-                >
-                  {name}
-                </Link>
-              );
-            })}
-          </div>
-        </nav>
+                return (
+                  <Link
+                    key={path}
+                    href={path}
+                    className={cn(
+                      "flex align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200",
+                      {
+                        "text-neutral-500": !isActive,
+                        "font-bold": isActive,
+                      }
+                    )}
+                  >
+                    <span className="relative py-[5px] px-[10px]">
+                      {name}
+                      {path === pathname ? (
+                        <motion.div
+                          className="absolute inset-0 z-[-1] rounded-md bg-neutral-100 dark:bg-neutral-800"
+                          layoutId="sidebar"
+                          transition={{
+                            type: "spring",
+                            stiffness: 350,
+                            damping: 30,
+                          }}
+                        />
+                      ) : null}
+                    </span>
+                  </Link>
+                );
+              })}
+            </div>
+          </nav>
+        </LayoutGroup>
       </div>
     </aside>
   );
