956993c9a5ab21f8d018ddc31baaccf861d7030b
Niels Bik <nielsrowinbik@gmail.com>
2023-11-30 11:23:01 +0100

    feat: hide blog when there are no posts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,19 +5,18 @@ import { LayoutGroup, motion } from "framer-motion";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import { allBlogs } from "contentlayer/generated";
 
-const navItems: {
-  [path: string]: string;
-} = {
+const navItems = {
   "/": "home",
   "/about": "about",
   "/blog": "blog",
-};
+} as const;
 
 export function Navbar() {
   let pathname = (usePathname() as string) || "/";
 
-  if (!navItems[pathname]) {
+  if (!Object.keys(navItems).includes(pathname)) {
     pathname = "/blog";
   }
 
@@ -30,37 +29,42 @@ export function Navbar() {
         >
           <div className="my-2 flex flex-row space-x-0 pr-10 md:mt-0 md:flex-col">
             <LayoutGroup>
-              {Object.entries(navItems).map(([path, name]) => {
-                const isActive = path === pathname;
+              {Object.entries(navItems)
+                .filter(([_, name]) => {
+                  if (name === "blog" && allBlogs.length === 0) return false;
+                  return true;
+                })
+                .map(([path, name]) => {
+                  const isActive = path === pathname;
 
-                return (
-                  <Link
-                    key={path}
-                    href={path}
-                    className={cn(
-                      "flex align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200",
-                      {
-                        "text-neutral-500": !isActive,
-                        "font-bold": isActive,
-                      }
-                    )}
-                  >
-                    <span className="relative py-[5px] px-[10px]">
-                      {name}
-                      {path === pathname ? (
-                        <motion.div
-                          className="absolute inset-0 z-[-1] rounded-md bg-neutral-100 dark:bg-neutral-800"
-                          layoutId="navbar"
-                          transition={{
-                            duration: 0.2,
-                            ease: "easeOut",
-                          }}
-                        />
-                      ) : null}
-                    </span>
-                  </Link>
-                );
-              })}
+                  return (
+                    <Link
+                      key={path}
+                      href={path}
+                      className={cn(
+                        "flex align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200",
+                        {
+                          "text-neutral-500": !isActive,
+                          "font-bold": isActive,
+                        }
+                      )}
+                    >
+                      <span className="relative py-[5px] px-[10px]">
+                        {name}
+                        {path === pathname ? (
+                          <motion.div
+                            className="absolute inset-0 z-[-1] rounded-md bg-neutral-100 dark:bg-neutral-800"
+                            layoutId="navbar"
+                            transition={{
+                              duration: 0.2,
+                              ease: "easeOut",
+                            }}
+                          />
+                        ) : null}
+                      </span>
+                    </Link>
+                  );
+                })}
             </LayoutGroup>
           </div>
         </nav>
