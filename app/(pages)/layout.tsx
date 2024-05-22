9f5fbacd3f8efb5cf6fc419984e5751c4e52089f
Niels <nielsrowinbik@users.noreply.github.com>
2024-05-22 13:58:51 +0200

    feat: use text-wrap pretty (#275)

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export default function PagesLayout({ children }: { children: ReactNode }) {
   return (
-    <section className="prose prose-neutral text-neutral-800 dark:prose-invert prose-headings:font-serif prose-h1:text-2xl prose-hr:my-6 prose-hr:border-neutral-100 dark:text-neutral-200 prose-hr:dark:border-neutral-800">
+    <section className="prose prose-neutral text-neutral-800 dark:prose-invert prose-headings:font-serif prose-h1:text-2xl prose-p:text-pretty prose-hr:my-6 prose-hr:border-neutral-100 dark:text-neutral-200 prose-hr:dark:border-neutral-800">
       {children}
     </section>
   );
